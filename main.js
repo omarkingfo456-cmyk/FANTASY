da468c3f470d7b5a0c4976fb5c610ca2f41dbc04
omarkingfo456-cmyk <omarkingfo456@gmail.com>
2026-09-10 12:31:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,35 @@
 function toggleTheme() {
     document.body.classList.toggle("dark");
 }
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const error = document.getElementById("error");
+
+    // بيانات الدخول
+    if (username === "admin" && password === "1234") {
+        error.textContent = "تم تسجيل الدخول بنجاح";
+
+        // الانتقال للصفحة الرئيسية
+        setTimeout(function () {
+            window.location.href = "home.html";
+        }, 500);
+
+    } else {
+        error.textContent = "اسم المستخدم أو كلمة المرور غير صحيحة";
+    }
+}
+
+function togglePassword() {
+    const password = document.getElementById("password");
+
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
