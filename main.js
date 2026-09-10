3f60a3f14038e635d4d73a0532a3a2d6bdda0da7
omarkingfo456-cmyk <omarkingfo456@gmail.com>
2026-09-10 12:41:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -52,3 +52,12 @@ function togglePassword() {
 function toggleTheme() {
     document.body.classList.toggle("dark");
 }
+function togglePassword() {
+    const password = document.getElementById("password");
+
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+}
