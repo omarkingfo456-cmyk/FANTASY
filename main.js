99867f83a09d274bb194e0524b40e30f86a8ce39
omarkingfo456-cmyk <omarkingfo456@gmail.com>
2026-09-10 14:47:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,66 +1,20 @@
-
-   function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const error = document.getElementById("error");
-
-    if (username === "") {
-        error.textContent = "من فضلك اختر المستخدم وأدخل كلمة المرور";
-        return;
-    }
-
-    if (password === "") {
-        error.textContent = "من فضلك أدخل كلمة المرور";
-        return;
-    }
-
-    error.textContent = "تم تسجيل الدخول بنجاح";
-}
-
-function toggleTheme() {
-    document.body.classList.toggle("dark");
-}
 function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const error = document.getElementById("error");
 
-    // بيانات الدخول
-    if (username === "Omar" && password === "123456") {
-        error.textContent = "تم تسجيل الدخول بنجاح";
+    if (username === "admin" && password === "1234") {
+        error.textContent = "✅ كلمة المرور صحيحة";
 
-        // الانتقال للصفحة الرئيسية
         setTimeout(function () {
             window.location.href = "home.html";
         }, 500);
 
     } else {
-        error.textContent = "اسم المستخدم أو كلمة المرور غير صحيحة";
+        error.textContent = "❌ كلمة المرور أو اسم المستخدم غلط";
     }
 }
 
-function togglePassword() {
-    const password = document.getElementById("password");
-
-    if (password.type === "password") {
-        password.type = "text";
-    } else {
-        password.type = "password";
-    }
-}
-
-function toggleTheme() {
-    document.body.classList.toggle("dark");
-}
-function togglePassword() {
-    const password = document.getElementById("password");
-
-    if (password.type === "password") {
-        password.type = "text";
-    } else {
-        password.type = "password";
-    }
-}
 function togglePassword() {
     const password = document.getElementById("password");
 
